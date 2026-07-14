63b35078395b72d95c7c5bd29325c33939ec37f7
Your Name <anjalisuthar114@gmail.com>
2026-07-14 18:36:23 +0530

    added file of logout/route.js

---
+++ app/api/auth/logout/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+
+    const response = NextResponse.json({
+        success:true,
+        message:"Logged Out"
+    });
+
+    response.cookies.set("token","",{
+        maxAge:0,
+        path:"/"
+    });
+
+    return response;
+
+}
